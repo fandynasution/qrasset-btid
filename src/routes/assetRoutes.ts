a1c8f618446d33305764d70626fd194b70ef08c5
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-11-15 14:04:08 +0700

    test add swagger

--- src/routes/assetRoutes.ts
+++ src/routes/assetRoutes.ts
@@ -4,7 +4,36 @@ import { DatanonQr, DatawithQr, DataWhere } from '../controllers/FaAssetControll
 
 const router = express.Router();
 
+
+
 router.get("/generate", generateAndSaveQrCode);
+/**
+ * @swagger
+ * /api/generate:
+ *   post:
+ *     summary: Login user
+ *     description: Authenticate user with email and password to get token
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "mgr@ifca.co.id"
+ *               password:
+ *                 type: string
+ *                 example: "1fc41fc4"
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *       401:
+ *         description: Invalid credentials
+ */
+
 router.get("/datanonqr", DatanonQr);
 router.get("/datawithqr", DatawithQr);
 router.post("/datawhere", DataWhere);
