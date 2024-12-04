b4fd8301cad1b8f239a709716464bc71d797b7c4
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-12-04 10:43:45 +0700

    OKAY

--- src/routes/assetRoutes.ts
+++ src/routes/assetRoutes.ts
@@ -7,58 +7,6 @@ const router = express.Router();
 
 
 
-router.get("/generate", generateAndSaveQrCode);
-/**
- * @swagger
- * /api/generate:
- *   get:
- *     summary: Generate QR Code
- *     description: Run this URL to Generate QR Code with condition data with NULL QR Code on DB
- *     tags: [For QR Code Dashboard]
- *     responses:
- *       200:
- *         description: Successful Generate all Data
- *       404:
- *         description: No Data on DB with Empty QR Code (already generate)
- *       500:
- *         description: Erro Connection to DB
- */
-
-router.get("/datanonqr", DatanonQr);
-/**
- * @swagger
- * /api/datanonqr:
- *   get:
- *     summary: Select QR Asset non QR
- *     description: Run this URL to View all Data that does not yet have a QR Code
- *     tags: [For QR Code Dashboard]
- *     responses:
- *       200:
- *         description: Successful Select Data 
- *       404:
- *         description: No Data on DB with Empty QR Code (already generate)
- *       500:
- *         description: Error Connection to DB
- */
-
-
-router.get("/datawithqr", DatawithQr);
-/**
- * @swagger
- * /api/datawithqr:
- *   get:
- *     summary: Select QR Asset with QR
- *     description: Run this URL to View all Data that has a QR Code
- *     tags: [For QR Code Dashboard]
- *     responses:
- *       200:
- *         description: Successful Select Data 
- *       404:
- *         description: No Data on DB with QR Code (dont generate yet)
- *       500:
- *         description: Error Connection to DB
- */
-
 router.post("/get-asset", DataWhere);
 /**
  * @swagger
@@ -92,14 +40,14 @@ router.post("/get-asset", DataWhere);
  *         description: Error Connection to DB
  */
 
-router.put('/update-print', DataUpdatePrint);
+router.post('/update-asset', UpdateAsset);
 /**
  * @swagger
- * /api/update-print:
- *   put:
- *     summary: Update QR Asset after print (allow single object or multiple array)
- *     description: Run this URL to update multiple QR Assets after print or a single asset.
-  *     tags: [For QR Code Dashboard]
+ * /api/update-asset:
+ *   post:
+ *     summary: Update QR Asset with specified parameters
+ *     description: Update QR Asset data by providing necessary parameters including location coordinates.
+ *     tags: [For QR Code Power Apps]
  *     requestBody:
  *       required: true
  *       content:
@@ -111,21 +59,40 @@ router.put('/update-print', DataUpdatePrint);
  *               properties:
  *                 entity_cd:
  *                   type: string
+ *                   description: Entity code identifier
  *                 reg_id:
  *                   type: string
+ *                   description: Registration ID of the asset
+ *                 location_map:
+ *                   type: string
+ *                   description: Geographical coordinates (latitude and longitude)
+ *                 status_review:
+ *                   type: string
+ *                   description: Status review identifier
+ *                 notes:
+ *                   type: string
+ *                   description: Additional notes about the asset
+ *                 audit_status:
+ *                   type: string
+ *                   description: Indicates if the asset has been audited ("Y" or "N")
+ *                 source_file_attachment:
+ *                   type: string
+ *                   description: Base64 encoded image file
+ *                   example: "data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAZABkAAD/..."
  *               example:
  *                 entity_cd: "01"
- *                 reg_id: "006/EQP2/BTID/VI/12"
+ *                 reg_id: "008/EQP2/BTID/VI/21"
+ *                 location_map: "-6.21462, 106.84513"
+ *                 status_review: "1"
+ *                 notes: "Catatan Untuk Asset BTID"
+ *                 audit_status: "Y"
+ *                 source_file_attachment: "data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAZABkAAD/..."
  *     responses:
  *       200:
- *         description: Successful Update Data
+ *         description: Successful update of asset data
  *       404:
- *         description: No Data on DB
+ *         description: No data found in the database
  *       500:
- *         description: Error Connection to DB
+ *         description: Error connecting to the database
  */
-
-router.get('/update-asset', UpdateAsset);
-
-
 export default router;
